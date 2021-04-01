736b2977572b8d1e4df46f503a9154733bcfe7c8
Dan Ring <dan.ring@me.com>
2021-04-01 21:41:56 +0900

    initial set up of seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,91 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "generating users..."
+# => user seeds
+User.new( {
+            first_name: 'Dan',
+            last_name: 'Ring',
+            email: 'dan@gmail.com'
+            password: 'foodcart1',
+            admin: true
+})
+
+User.new( {
+            first_name: 'Hongjoo',
+            last_name: 'Yang',
+            email: 'hongjoo@gmail.com',
+            password: 'foodcart2',
+            admin: true
+})
+
+User.new( {
+            first_name: 'Josh',
+            last_name: 'Hume',
+            email: 'josh@gmail.com',
+            password: 'foodcart3',
+            admin: true
+})
+
+User.new( {
+            first_name: 'Marisa',
+            last_name: 'Cassidy',
+            email: 'marisa@gmail.com',
+            password: 'foodcart4',
+            admin: true
+})
+
+User.new( {
+            first_name: 'Brandon',
+            last_name: 'Lindberg',
+            email: 'brandon@gmail.com',
+            password: 'foodcart5',
+            admin: true
+})
+
+puts "done"
+puts "generating foodcarts"
+
+20.times do
+  FoodCart.new({
+                 name: Faker::Restaurant.name,
+                 category: Faker::Restaurant.type,
+                 open: true,
+                 cart_description: Faker::Restaurant.description,
+                 user: User.all.sample,
+                 menu: Menu.all.sample
+  })
+end
+
+puts "done"
+puts "generating food items"
+
+20.times do
+  FoodItem.new({
+                 food_name: Faker::Food.dish,
+                 food_price: [1..10].sample,
+                 food_description: Faker::Food.description,
+                 food_type: %w[food drink].sample,
+                 food_availability: [true, false].sample
+
+  })
+end
+
+puts "done"
+puts "generating menus"
+
+20.times do
+  Menu.new({
+             food_items: FoodItem.all(sample)
+  })
+end
+
+20.times do
+  Schedule.new({
+                 location: lat:f.rand lon:f.rand,
+                 date: Faker::Date.between(from: '2020-09-23', to: '2020-09-25'),
+                 start_time: w%[0..23],
+                 end_time: w%[0..23],
+  })
+end
