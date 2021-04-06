04b252e099f6a4b735378056976e2984d1f9db30
Dan Ring <dan.ring@me.com>
2021-04-06 21:58:58 +0900

    updated the seeds with relevant items

--- app/models/food_cart.rb
+++ app/models/food_cart.rb
@@ -2,9 +2,10 @@ class FoodCart < ApplicationRecord
   has_many :users
   belongs_to :user
   has_one :schedule
+  has_one :menu
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :category, presence: true
-  validates :open, default: false
+  # validates :open, default: false
   validates :cart_description, presence: true
 end
