2027b7e81298c6a6e9daa6596cb403ff4dd7cf1f
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-04-13 20:35:12 +0900

    quick style update and comments added in the Menu Controller

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -43,12 +43,14 @@ class MenuController < ApplicationController
   end
 
   private
+
   def menu_params
     params.require(:menu).permit()
   end
 
-    def set_menu
-      @menu = Menu.find(params[:id])
+  #method for authorization in the before action
+  def set_menu
+    @menu = Menu.find(params[:id])
       authorize @menu
-    end
+  end
 end
