fd9442bdd726b126622a12a782ee5208e4c50861
Joshua Hume <valencelectron@hotmail.com>
2021-04-22 21:09:15 +0900

    converted food_carts#show to React

--- app/javascript/packs/FoodCarts/show.jsx
+++ app/javascript/packs/FoodCarts/show.jsx
@@ -1,17 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 const FoodCart = () => {
   const [foodCart, setFoodCart] = useState(window.foodCart);
+  const [schedule, setSchedule] = useState(window.schedule);
+  const [menu, setMenu] = useState(window.menu);
+  const [foodItems, setFoodItems] = useState(window.foodItems);
   const [textContent, setTextContent] = useState('I am a food cart! Click me!');
-  const handleClick = () => {
-    textContent === 'I am a food cart!🚂 Click me!'
-      ? setTextContent('I am onigiri! 🍙 Click me!')
-      : setTextContent('I am a food cart!🚂 Click me!');
-  };
+  console.log(foodItems);
   return (
-    <div className="FoodCart" onClick={handleClick}>
-      {textContent}
-      {foodCart.map((data) => JSON.stringify(data))}
+    <div className="FoodCart">
+      <p>Open: {schedule.start_time}</p>
+      <p>Close: {schedule.end_time}</p>
+      <ul>
+        {foodItems.map((item, index) => (
+          <li key={index}>
+            {item.food_name} | JPY {item.food_price} 00
+          </li>
+        ))}
+      </ul>
+      <a href={`/schedules/${foodCart.id}`}>Show Schedule</a>
     </div>
   );
 };
