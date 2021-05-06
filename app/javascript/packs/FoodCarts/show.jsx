4611351f6c7c6c6fe592e065f5fd678300a32b5d
Hongjoo Yang <attinore@gmail.com>
2021-05-06 20:51:52 +0900

    the menu selection button working

--- app/javascript/packs/FoodCarts/show.jsx
+++ app/javascript/packs/FoodCarts/show.jsx
@@ -1,20 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import FoodItemCard from '../FoodItems/foodItemCard';
 
 const root = document.getElementById('root');
 
 const FoodCart = ({ foodCart }) => {
+  const [ userSelection, setUserSelection ] = useState(null);
   console.log(foodCart, 'YOOOO');
   const schedule = foodCart.schedule;
   const menu = foodCart.menu;
   const foodItems = foodCart.food_items;
   console.log(foodItems);
-  function handleFoodTypeClick(type) {
-    // if food is = food only show food, if it's drink only show drink
-    console.log(type);
+  useEffect(() => {
+    console.log(userSelection);
+  }, [ userSelection ]);
+  function getFoodItemCard (item, index){
+    switch (userSelection) {
+      case 'food':
+        if (item.food_type==='food') {
+        return <FoodItemCard
+        key={index}
+        id={item.id}
+        name={item.food_name}
+        description={item.food_description}
+        type={item.food_type}
+        price={item.food_price}
+        availability={item.food_availability}
+      />;
+    } else {
+      return ''
+    }
+      case 'drink':
+        if (item.food_type==='drink') {
+        return <FoodItemCard
+        key={index}
+        id={item.id}
+        name={item.food_name}
+        description={item.food_description}
+        type={item.food_type}
+        price={item.food_price}
+        availability={item.food_availability}
+      />;
+        } else {
+          return ''
+        }
+      default:
+        return <FoodItemCard
+        key={index}
+        id={item.id}
+        name={item.food_name}
+        description={item.food_description}
+        type={item.food_type}
+        price={item.food_price}
+        availability={item.food_availability}
+      />;
+    }
   }
 
   return (
@@ -22,25 +64,13 @@ const FoodCart = ({ foodCart }) => {
       <p>Open: {schedule.start_time}</p>
       <p>Close: {schedule.end_time}</p>
       <div>
-        <button onClick={() => handleFoodTypeClick('food')}>Food</button>
-        <button onClick={() => handleFoodTypeClick('drink')}>Drinks</button>
+        <button onClick={() => setUserSelection(null)}>All</button>
+        <button onClick={() => setUserSelection('food')}>Food</button>
+        <button onClick={() => setUserSelection('drink')}>Drinks</button>
       </div>
 
       <div>
-        {foodItems.map((item, index) =>
-          item.food_availability ? (
-            <FoodItemCard
-              key={index}
-              id={item.id}
-              name={item.food_name}
-              description={item.food_description}
-              type={item.food_type}
-              price={item.food_price}
-              availability={item.food_availability}
-            />
-          ) : (
-            ''
-          )
+        {foodItems.map((item, index) => getFoodItemCard(item, index)
         )}
       </div>
       <a href={`/schedules/${foodCart.id}`}>Show Schedule</a>
