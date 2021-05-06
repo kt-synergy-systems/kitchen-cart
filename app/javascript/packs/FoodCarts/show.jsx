fae93629164f26b56e5c75f549be288a1970fb7a
Joshua Hume <valencelectron@hotmail.com>
2021-05-06 22:05:30 +0900

    fixed loading issue

--- app/javascript/packs/FoodCarts/show.jsx
+++ app/javascript/packs/FoodCarts/show.jsx
@@ -7,7 +7,7 @@ import FoodItemCard from '../FoodItems/foodItemCard';
 const root = document.getElementById('root');
 
 const FoodCart = ({ foodCart }) => {
-  const [ userSelection, setUserSelection ] = useState(null);
+  const [userSelection, setUserSelection] = useState(null);
   console.log(foodCart, 'YOOOO');
   const schedule = foodCart.schedule;
   const menu = foodCart.menu;
@@ -15,47 +15,53 @@ const FoodCart = ({ foodCart }) => {
   console.log(foodItems);
   useEffect(() => {
     console.log(userSelection);
-  }, [ userSelection ]);
-  function getFoodItemCard (item, index){
+  }, [userSelection]);
+  function getFoodItemCard(item, index) {
     switch (userSelection) {
       case 'food':
-        if (item.food_type==='food') {
-        return <FoodItemCard
-        key={index}
-        id={item.id}
-        name={item.food_name}
-        description={item.food_description}
-        type={item.food_type}
-        price={item.food_price}
-        availability={item.food_availability}
-      />;
-    } else {
-      return ''
-    }
-      case 'drink':
-        if (item.food_type==='drink') {
-        return <FoodItemCard
-        key={index}
-        id={item.id}
-        name={item.food_name}
-        description={item.food_description}
-        type={item.food_type}
-        price={item.food_price}
-        availability={item.food_availability}
-      />;
+        if (item.food_type === 'food') {
+          return (
+            <FoodItemCard
+              key={index}
+              id={item.id}
+              name={item.food_name}
+              description={item.food_description}
+              type={item.food_type}
+              price={item.food_price}
+              availability={item.food_availability}
+            />
+          );
         } else {
-          return ''
+          return '';
+        }
+      case 'drink':
+        if (item.food_type === 'drink') {
+          return (
+            <FoodItemCard
+              key={index}
+              id={item.id}
+              name={item.food_name}
+              description={item.food_description}
+              type={item.food_type}
+              price={item.food_price}
+              availability={item.food_availability}
+            />
+          );
+        } else {
+          return '';
         }
       default:
-        return <FoodItemCard
-        key={index}
-        id={item.id}
-        name={item.food_name}
-        description={item.food_description}
-        type={item.food_type}
-        price={item.food_price}
-        availability={item.food_availability}
-      />;
+        return (
+          <FoodItemCard
+            key={index}
+            id={item.id}
+            name={item.food_name}
+            description={item.food_description}
+            type={item.food_type}
+            price={item.food_price}
+            availability={item.food_availability}
+          />
+        );
     }
   }
 
@@ -69,18 +75,13 @@ const FoodCart = ({ foodCart }) => {
         <button onClick={() => setUserSelection('drink')}>Drinks</button>
       </div>
 
-      <div>
-        {foodItems.map((item, index) => getFoodItemCard(item, index)
-        )}
-      </div>
+      <div>{foodItems.map((item, index) => getFoodItemCard(item, index))}</div>
       <a href={`/schedules/${foodCart.id}`}>Show Schedule</a>
     </div>
   );
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <FoodCart foodCart={JSON.parse(root.dataset.foodCart)} />,
-    root
-  );
-});
+ReactDOM.render(
+  <FoodCart foodCart={JSON.parse(root.dataset.foodCart)} />,
+  root
+);
