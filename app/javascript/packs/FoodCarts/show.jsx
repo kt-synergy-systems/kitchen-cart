1aa3855fd219a9c71eb50a4ce1ad95d4f11e231f
Hongjoo Yang <attinore@gmail.com>
2021-05-13 20:18:29 +0900

    moved the picture+location_tag element to food cart show page from foodCartCard.jsx, which was a long file

--- app/javascript/packs/FoodCarts/show.jsx
+++ app/javascript/packs/FoodCarts/show.jsx
@@ -15,27 +15,32 @@ const FoodCart = ({ foodCart }) => {
   useEffect(() => {
     console.log(userSelection);
   }, [userSelection]);
-
   return (
-    <div className='FoodCart'>
-      {schedules.map((schedule) => (
-        <>
-          <p>Date: {schedule.date}</p>
-          <p>Open: {schedule.start_time}</p>
-          <p>Close: {schedule.end_time}</p>
-        </>
-      ))}
+  <div>
+    <div>
+      <div className="location-tag"><i className="fas fa-map-marker-alt"></i>{" "}Location name</div>
+      <img src="https://images.unsplash.com/photo-1565123409695-7b5ef63a2efb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80" alt="alttext" className="food_cart-image" />
+    </div>
+      <div className='FoodCart'>
+        {schedules.map((schedule) => (
+          <>
+            <p>Date: {schedule.date}</p>
+            <p>Open: {schedule.start_time}</p>
+            <p>Close: {schedule.end_time}</p>
+          </>
+        ))}
 
-      <img src='' alt='' />
-      <div className='icons'></div>
-      <div className='foodcart-intro'></div>
-      <div>
-        <button onClick={() => setUserSelection(null)}>All</button>
-        <button onClick={() => setUserSelection('food')}>Food</button>
-        <button onClick={() => setUserSelection('drink')}>Drinks</button>
+        <img src='' alt='' />
+        <div className='icons'></div>
+        <div className='foodcart-intro'></div>
+        <div>
+          <button onClick={() => setUserSelection(null)}>All</button>
+          <button onClick={() => setUserSelection('food')}>Food</button>
+          <button onClick={() => setUserSelection('drink')}>Drinks</button>
+        </div>
+
+        <div>{foodItems.map((item, index) => getFoodItemCard(item, index, userSelection))}</div>
       </div>
-
-      <div>{foodItems.map((item, index) => getFoodItemCard(item, index, userSelection))}</div>
     </div>
   );
 };
