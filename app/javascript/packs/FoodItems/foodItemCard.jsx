d762f320ca5e12b2709266fcdeecb04615081276
Marisa Cassidy <marisancassidy@gmail.com>
2021-05-13 21:50:54 +0900

    Add css styling for buttons

--- app/javascript/packs/FoodItems/foodItemCard.jsx
+++ app/javascript/packs/FoodItems/foodItemCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FoodItemCard = ({name, type, description, price, availability}) => {
   return (
     <div className="food-item-card">
-      <img className="food-item-image" src="" alt="" />
+      <img className="food-item-image" src="https://picsum.photos/400/300" alt="Lorem Picsum" />
       <div className="food-item-text">
         <h6>{name}</h6>
         <p>{description}</p>
