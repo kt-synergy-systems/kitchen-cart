a268810d433f0085105c7304b9e82a82af52ec12
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-07-20 21:03:18 +0900

    added require tie to application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 require 'yaml'
 require 'rails/all'
-
+require "action_mailer/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
