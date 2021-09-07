34a2531008810978d6fe36352451b7489bcb2bd5
Joshua Hume <valencelectron@hotmail.com>
2021-09-07 21:03:03 +0900

    pushing to master like a real man 💪

--- app/javascript/packs/MainTemplate.jsx
+++ app/javascript/packs/MainTemplate.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Home from "./Pages/Home";
-import FoodCarts from "./FoodCarts/index";
-import FoodCart from "./FoodCarts/Show";
-import Menus from "./Menus/index";
-import Schedules from "./Schedules/index";
-import NewFoodCart from "./FoodCarts/New";
-import NewFoodItem from "./FoodItems/New";
+import React from 'react';
+import Home from './Pages/Home';
+import FoodCarts from './FoodCarts/index';
+import FoodCart from './FoodCarts/Show';
+import Menus from './Menus/index';
+import Schedules from './Schedules/index';
+import NewFoodCart from './FoodCarts/New';
+import NewFoodItem from './FoodItems/New';
 
 const MainTemplate = ({ data, contentType }) => {
   const getCurrentPage = () => {
     switch (contentType) {
-      case "FOOD_CARTS":
+      case 'FOOD_CARTS':
         return (
           <FoodCarts
             foodCarts={data.food_carts}
@@ -19,17 +19,17 @@ const MainTemplate = ({ data, contentType }) => {
             likes={data.likes}
           />
         );
-      case "NEW_FOOD_CART":
+      case 'NEW_FOOD_CART':
         return <NewFoodCart foodCart={data.food_cart} user={data.user} />;
-      case "EDIT_FOOD_CART":
+      case 'EDIT_FOOD_CART':
         return (
           <NewFoodCart foodCart={data.food_cart} user={data.user} edit={true} />
         );
-      case "HOME":
+      case 'HOME':
         return <Home />;
-      case "MENUS":
+      case 'MENUS':
         return <Menus menus={data} user={data.user} />;
-      case "FOOD_CART":
+      case 'FOOD_CART':
         return (
           <FoodCart
             foodCart={JSON.parse(data.food_cart)}
@@ -39,7 +39,7 @@ const MainTemplate = ({ data, contentType }) => {
             photos={data.photos}
           />
         );
-      case "SCHEDULES":
+      case 'SCHEDULES':
         return (
           <Schedules
             schedules={data.schedules}
@@ -47,11 +47,11 @@ const MainTemplate = ({ data, contentType }) => {
             user={data.user}
           />
         );
-      case "NEW_FOOD_ITEM":
+      case 'NEW_FOOD_ITEM':
         return (
           <NewFoodItem foodItem={JSON.parse(data.food_item)} user={data.user} />
         );
-      case "EDIT_FOOD_ITEM":
+      case 'EDIT_FOOD_ITEM':
         return (
           <NewFoodItem
             foodItem={JSON.parse(data.food_item)}
@@ -60,7 +60,7 @@ const MainTemplate = ({ data, contentType }) => {
           />
         );
       default:
-        return "";
+        return '';
     }
   };
   return <div>{getCurrentPage()}</div>;
