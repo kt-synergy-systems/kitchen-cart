4c8a765494a50f3f76476638916db91ab28e0081
brandon lindberg <lindberg.scott.brandon@gmail.com>
2021-10-06 21:46:22 +0900

    updated the localisation files with react

--- app/javascript/packs/Pages/Title.jsx
+++ app/javascript/packs/Pages/Title.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import KitchenCartLogo from '../../../assets/images/kitchen-cart-logo.png';
+import { useTranslation } from "react-i18next";
 
 const Title = () => {
+  const translateThis = useTranslation()
+  console.log(translateThis)
   return (
     <div className='home-container'>
       <div className='home-logo'>
         <img 
           src={KitchenCartLogo} alt='Kitchen Cart logo'
           />
-        <h1><strong>Kitchen Cart</strong></h1>
+        <h1><strong>{translateThis.t("title")}</strong></h1>
       </div>
       <div className='search-container'></div>
       <div className='main-intro-card'>
