16c8f3ad8dd8b21a97f4da6fd002ace594bbad89
Joshua Hume <valencelectron@hotmail.com>
2021-10-26 21:52:16 +0900

    ”commiting＿with＿stupid_message

--- app/javascript/packs/Schedules/new.js
+++ app/javascript/packs/Schedules/new.js
@@ -1,35 +1,55 @@
-import mapboxgl from "!mapbox-gl";
-import i18next from "i18next";
-import { translationData } from "../entry";
+import mapboxgl from '!mapbox-gl';
+import i18next from 'i18next';
+import { translationData } from '../entry';
+console.log(translationData);
+console.log(i18next);
 i18next.init(translationData);
 const t = i18next.t.bind(i18next);
+window.i18n = i18next;
 window.t = t;
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
-const dataDiv = document.getElementById("data-div");
+const dataDiv = document.getElementById('data-div');
 const data = dataDiv ? JSON.parse(dataDiv.dataset.data) : null;
 console.log(data);
-const searchForm = document.getElementById("search-form");
-const searchInput = document.getElementById("search-input");
-const locationInput = document.getElementById("schedule_location");
-const longitudeInput = document.getElementById("schedule_longitude");
-const latitudeInput = document.getElementById("schedule_latitude");
-const simpleForm = document.querySelector(".simple_form");
-const simpleFormBtn = document.getElementById("simple-form-btn");
-const endTimeInput = document.getElementById("schedule_end_time");
-const startTimeInput = document.getElementById("schedule_start_time");
-const dateInput = document.getElementById("schedule_date");
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const locationInput = document.getElementById('schedule_location');
+const longitudeInput = document.getElementById('schedule_longitude');
+const latitudeInput = document.getElementById('schedule_latitude');
+const simpleForm = document.querySelector('.simple_form');
+const simpleFormBtn = document.getElementById('simple-form-btn');
+const butt = document.getElementById('simple-form-btn');
+const endTimeInput = document.getElementById('schedule_end_time');
+const startTimeInput = document.getElementById('schedule_start_time');
+const dateInput = document.getElementById('schedule_date');
+const mapInstructions = document.querySelector('.map-instructions');
+const searchBtn = document.getElementById('search-btn');
+const locationText = document.querySelector('.location-text');
+const startTimeText = document.querySelector('.start-time-text');
+const endTimeText = document.querySelector('.end-time-text');
+const scheduleLocation = document.getElementById('schedule_location');
+const scheduleTitle = document.getElementById('schedule-title');
+
+mapInstructions.innerText = t('forms.schedule_form.instruction');
+searchInput.placeholder = t('forms.schedule_form.search_text');
+searchBtn.innerText = t('buttons.search');
+locationText.innerText = t('forms.schedule_form.location');
+startTimeText.innerText = t('forms.schedule_form.open');
+endTimeText.innerText = t('forms.schedule_form.close');
+scheduleLocation.placeholder = t('forms.schedule_form.nearest_station');
+scheduleTitle.innerText = t('forms.schedule_form.title');
+console.log(butt.innerText);
+butt.value = t('buttons.create_schedule');
 let marker;
 let lang;
 console.log(searchForm);
 
 const getFormattedTime = (date) => {
-  return date.toISOString().match(/\d{4}-\d\d-\d\dT\d\d:/)[0] + "00";
+  return date.toISOString().match(/\d{4}-\d\d-\d\dT\d\d:/)[0] + '00';
 };
 const initialTime = new Date(Date.now() + 1000 * 60 * 60 * 10);
-startTimeInput.value = data
-  ? getFormattedTime(new Date(data.schedule.start_time))
-  : getFormattedTime(initialTime);
+startTimeInput.value = data ? getFormattedTime(new Date(data.schedule.start_time)) : getFormattedTime(initialTime);
 startTimeInput.min = getFormattedTime(initialTime);
 endTimeInput.min = getFormattedTime(initialTime);
 endTimeInput.value = data
@@ -38,40 +58,37 @@ endTimeInput.value = data
 
 dateInput.value = new Date(startTimeInput.value).toDateString();
 
-startTimeInput.addEventListener("change", (e) => {
+startTimeInput.addEventListener('change', (e) => {
   simpleFormBtn.disabled = false;
   endTimeInput.min = startTimeInput.value;
   endTimeInput.value = startTimeInput.value;
   dateInput.value = new Date(startTimeInput.value).toDateString();
 });
-endTimeInput.addEventListener("change", () => (simpleFormBtn.disabled = false));
+endTimeInput.addEventListener('change', () => (simpleFormBtn.disabled = false));
 
-if (navigator.language === "ja" || navigator.language.includes("en")) {
-  lang = navigator.language.includes("en") ? "en" : navigator.language;
+if (navigator.language === 'ja' || navigator.language.includes('en')) {
+  lang = navigator.language.includes('en') ? 'en' : navigator.language;
 } else if (navigator.languages && navigator.languages[0]) {
-  if (
-    navigator.languages[0].includes("en") ||
-    navigator.languages[0] === "ja"
-  ) {
-    lang = navigator.languages[0].includes("en") ? "en" : "ja";
+  if (navigator.languages[0].includes('en') || navigator.languages[0] === 'ja') {
+    lang = navigator.languages[0].includes('en') ? 'en' : 'ja';
   } else {
-    lang = "en";
+    lang = 'en';
   }
 } else {
-  lang = "en";
+  lang = 'en';
 }
 console.log(lang);
 
 const map = new mapboxgl.Map({
-  container: "map", // container id
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/streets-v11', // style URL
   center: [139.7528, 35.6852], // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
 
 const createMarker = (lng, lat) => {
-  const el = document.createElement("div");
-  el.className = "marker";
+  const el = document.createElement('div');
+  el.className = 'marker';
   marker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
 };
 
@@ -91,7 +108,7 @@ const setCoords = (lng, lat) => {
   latitudeInput.value = lat;
 };
 
-map.on("click", async (e) => {
+map.on('click', async (e) => {
   removeMarker();
   let lng;
   let lat;
@@ -110,7 +127,7 @@ map.on("click", async (e) => {
   console.log(data);
 });
 
-searchForm.addEventListener("submit", async (e) => {
+searchForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   const res = await fetch(
     `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchInput.value}.json?&country=jp&proximity=139.76,35.68&access_token=${mapboxgl.accessToken}`
@@ -128,34 +145,26 @@ searchForm.addEventListener("submit", async (e) => {
   }
 });
 
-simpleForm.addEventListener("submit", (e) => {
-  if (
-    !latitudeInput.value ||
-    !longitudeInput.value ||
-    latitudeInput.value === "" ||
-    longitudeInput.value === ""
-  ) {
+simpleForm.addEventListener('submit', (e) => {
+  if (!latitudeInput.value || !longitudeInput.value || latitudeInput.value === '' || longitudeInput.value === '') {
     e.preventDefault();
-    alert(t("alerts.geolocation"));
+    alert(t('alerts.geolocation'));
     simpleFormBtn.disabled = false;
-  } else if (dateInput.value === "" || !dateInput.value) {
+  } else if (dateInput.value === '' || !dateInput.value) {
     e.preventDefault();
-    alert(t("alerts.start_time"));
+    alert(t('alerts.start_time'));
     simpleFormBtn.disabled = false;
-  } else if (endTimeInput.value === "" || !endTimeInput.value) {
+  } else if (endTimeInput.value === '' || !endTimeInput.value) {
     e.preventDefault();
-    alert(t("alerts.end_time"));
+    alert(t('alerts.end_time'));
     simpleFormBtn.disabled = false;
-  } else if (
-    new Date(endTimeInput.value) - new Date(startTimeInput.value) <=
-    0
-  ) {
+  } else if (new Date(endTimeInput.value) - new Date(startTimeInput.value) <= 0) {
     e.preventDefault();
-    alert(t("alerts.early_end"));
+    alert(t('alerts.early_end'));
     simpleFormBtn.disabled = false;
-  } else if (locationInput.value === "") {
+  } else if (locationInput.value === '') {
     e.preventDefault();
-    alert(t("alerts.location"));
+    alert(t('alerts.location'));
     simpleFormBtn.disabled = false;
   }
 });
